5733a99c020f7d6a50cb900b2cbce0ea27c9fba0
PatriotRossii <patriotrossii2019@mail.ru>
2023-02-26 22:13:04 +0300

    Implement registration endpoint

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,9 @@ from .models import Article
 from django.shortcuts import render, redirect
 from django.http import Http404
 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
 
 def archive(request):
 	return render(request, "archive.html", {"posts": Article.objects.all()})
@@ -34,3 +37,24 @@ def create_post(request):
 			return render(request, "create_post.html")
 	else:
 		raise Http404
+
+
+def register(request):
+	if request.user.is_anonymous:
+		if request.method == "POST":
+			form = {
+				"username": request.POST["username"], "email": request.POST["email"],
+				"password": request.POST["password"]
+			}
+			if form["username"] and form["email"] and form["password"]:
+				if User.objects.filter(username=form["username"]).exists():
+					form["errors"] = "Пользователь с таким именем пользователя уже существует"
+					return render(request, "registration.html", {"form": form})
+				login(request, User.objects.create_user(form["username"], form["email"], form["password"]))
+				return redirect("archive")
+			else:
+				form["errors"] = "Не все поля заполнены"
+				return render(request, "registration.html", {"form": form})
+		else:
+			return render(request, "registration.html")
+	return redirect("archive")
--- blog/urls.py
+++ blog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.archive, name="archive"),
     path('article/<int:article_id>', views.get_article, name='get_article'),
     path('article/add/', views.create_post, name='create_post'),
+    path('user/register/', views.register, name='register'),
 ]
